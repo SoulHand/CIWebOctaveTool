d16f57c016cfef6517c21529ee04bd8b853e0f99
matajmSoluciones <solucionesmatajm@gmail.com>
2016-11-01 05:53:25 -0400

    reparado error NaN

--- computeCost.m
+++ computeCost.m
@@ -3,6 +3,7 @@ function J = computeCost(X, y, theta)
 	H=sigmoid(X*theta);
 	[v,n]=size(H);
 	cost= sum((-y.*log(H))-((1.-y).*log(1.-H)));
+	%cost= sum((y-H).^2);
 	J = (1/(2*m))*cost;
 	J=(1/length(J))*sum(J);
 end
