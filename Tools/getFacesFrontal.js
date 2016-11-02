1f94b1fcf0653b8d9b224cf146d801597c87a95d
matajmSoluciones <solucionesmatajm@gmail.com>
2016-11-02 06:19:19 -0400

    extractores de rostros

---
+++ Tools/getFacesFrontal.js
@@ -0,0 +1,32 @@
+var fs= require('fs');
+var cv = require('opencv');
+const __DIR='./images/faces/';
+const __DIR_DESTINY='./images/FACE_FRONTAL/';
+var filesDirects=[],i=0;
+fs.readdir(__DIR, function(error,files){
+	if(error){
+		throw new Error(error);
+	}
+	filesDirects=files;
+	readFile();
+});
+function readFile(){
+	if(filesDirects[i]){
+		cv.readImage(__DIR+filesDirects[i], function(err, im) {
+				  if (err) throw err;
+				  if (im.width() < 1 || im.height() < 1) throw new Error('Image has no size');
+				  im.detectObject('./Tools/haarcascade_frontalface_alt2.xml', {}, function(err, faces) {
+				    if (!err){
+					    for (var j = 0,m=faces.length; j < m; j++) {
+					      	face = faces[j];
+					    	var img = im.crop(face.x, face.y, face.width, face.height);				    	
+					    	img.save(__DIR_DESTINY+j+"_"+filesDirects[i]);
+					    	console.log("Image saved to "+__DIR_DESTINY+j+"_"+filesDirects[i]);
+					    }				    	
+				    }
+				    i++;
+				    readFile();
+				  });
+		});
+	}
+}
